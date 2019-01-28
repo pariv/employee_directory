4904c6533b1c71ab4222c4830015f08a6005f2c6
pariv <pariv@local.yurion.ru>
2019-01-28 14:18:41 +0300

    Failing test when destroying related items during update

--- spec/api/v1/departments/update_spec.rb
+++ spec/api/v1/departments/update_spec.rb
@@ -28,4 +28,39 @@ RSpec.describe "departments#update", type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  describe 'update with removing notes' do
+    let!(:department) { create(:department) }
+    let!(:notes) {create_list(:department_note, 5, notable: department)}
+
+    let(:payload) do
+      {
+        data: {
+          id: department.id.to_s,
+          type: 'departments',
+          attributes: {
+            name: 'changed!'
+          },
+          relationships: {
+              "notes":{
+                  data:[
+                      {
+                          type: 'notes',
+                          id: notes.first.id.to_s,
+                          method: 'destroy'
+                      }
+                  ]
+              }
+          },
+        }
+      }
+    end
+
+    it 'removes note' do
+      expect(DepartmentResource).to receive(:find).and_call_original
+      expect(department.notes.count).to eq(5)
+      expect{make_request}.to change {department.notes.count}.by(-1)
+      expect(response.status).to eq(200)
+    end
+  end
 end
